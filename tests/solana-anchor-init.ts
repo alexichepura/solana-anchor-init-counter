7e74c14a97a43d2374b5e692014667ed1d806521
Alexi Chepura <a.chepura@gmail.com>
2022-01-23 13:27:12 +0300

    test Creates a counter

--- tests/solana-anchor-init.ts
+++ tests/solana-anchor-init.ts
@@ -1,17 +1,25 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolanaAnchorInit } from "../target/types/solana_anchor_init";
+const { SystemProgram } = anchor.web3;
+const { BN } = anchor;
+
+type TCounterProgram = Program<SolanaAnchorInit>;
 
 describe("solana-anchor-init", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.SolanaAnchorInit as TCounterProgram;
+  const counter = anchor.web3.Keypair.generate();
 
-  const program = anchor.workspace
-    .SolanaAnchorInit as Program<SolanaAnchorInit>;
-
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  it("Creates a counter", async () => {
+    await program.rpc.initialize(new BN(0), {
+      accounts: {
+        counter: counter.publicKey,
+        authority: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [counter],
+    });
   });
 });
