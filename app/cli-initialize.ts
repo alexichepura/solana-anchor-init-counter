d22fd1750131e86d18ab6a478dd1fe5b2c74ca48
Alexi Chepura <a.chepura@gmail.com>
2022-01-26 10:30:39 +0300

    cli init

---
+++ app/cli-initialize.ts
@@ -0,0 +1,24 @@
+import * as anchor from "@project-serum/anchor"
+import { Program } from "@project-serum/anchor"
+import { SolanaAnchorInit } from "../target/types/solana_anchor_init"
+const { SystemProgram } = anchor.web3
+const { BN } = anchor
+type TCounterProgram = Program<SolanaAnchorInit>
+
+const provider = anchor.Provider.env()
+anchor.setProvider(provider)
+const program = anchor.workspace.SolanaAnchorInit as TCounterProgram
+const counter = anchor.web3.Keypair.generate()
+
+const initialize = async () => {
+  await program.rpc.initialize(new BN(0), {
+    accounts: {
+      counter: counter.publicKey,
+      authority: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [counter],
+  })
+}
+
+initialize()
